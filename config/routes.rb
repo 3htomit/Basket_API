cf2322d0ffd316051f745670902003583afbd16d
TimothÃee Boiteau <timotheeboiteau@gmail.com>
2022-06-07 17:24:03 +0200

    Adding routes for the Basket show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      resources :items, only: [ :index, :show, :create ]
+      resources :items, only: %i[index show create]
+      resources :baskets, only: :show
     end
   end
 end

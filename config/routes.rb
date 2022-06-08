0f7fdd31a5b24c4b0509f6805e19f5bdc5a20826
TimothÃee Boiteau <timotheeboiteau@gmail.com>
2022-06-08 17:37:36 +0200

    Adding Baskets index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       resources :items, only: %i[index show create]
-      resources :baskets, only: :show
+      resources :baskets, only: %i[index show]
     end
   end
 end

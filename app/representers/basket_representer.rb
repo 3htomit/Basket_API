4fb89ab3c492985bacc4085313b0ea9b9be2d7ae
TimothÃee Boiteau <timotheeboiteau@gmail.com>
2022-06-08 11:43:16 +0200

    Updated BasketRep. to accept optional parameters in basket_calculator

--- app/representers/basket_representer.rb
+++ app/representers/basket_representer.rb
@@ -3,19 +3,14 @@ class BasketRepresenter
     @basket = basket
   end
 
-  def basket_calculator(items, options = {})
+  def basket_calculator(items, **options)
     total = 0
     items.each do |item|
       total += item.price * item.quantity
     end
-    # puts total
     total *= (1 - (options[:discount] || 0))
-    # puts options[:discount]
-    # puts total
     total *= 1.1
-    # puts total
     total += (options[:shipment] || 5)
-    # puts total.to_i
     total = total.to_i
   end
 
@@ -31,7 +26,7 @@ class BasketRepresenter
         quantity: item.quantity
       })
     end
-    basket_data[:total] = basket_calculator(basket.items, options: { discount: 0.1, shipment: 10 })
+    basket_data[:total] = basket_calculator(basket.items)
     basket_data
   end
 

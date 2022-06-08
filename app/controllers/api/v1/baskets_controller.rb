7dcb454abff3df51005c492db36d4fd67adb52be
TimothÃee Boiteau <timotheeboiteau@gmail.com>
2022-06-08 17:34:52 +0200

    Adding index of baskets

--- app/controllers/api/v1/baskets_controller.rb
+++ app/controllers/api/v1/baskets_controller.rb
@@ -1,6 +1,12 @@
 module Api
   module V1
     class BasketsController < ApplicationController
+      def index
+        baskets = Basket.all
+
+        render json: BasketsRepresenter.new(baskets).as_json
+      end
+
       def show
         basket = Basket.find(params[:id])
 

5ff72dbf1fcff382dc736bb2e3312c03640922c5
TimothÃee Boiteau <timotheeboiteau@gmail.com>
2022-06-09 13:27:29 +0200

    Updated seeds to take into account the new optional discount and shipment variables

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,22 +16,30 @@ puts '   > item 3'
 item3 = Item.create(price: 5, quantity: 4)
 
 
-puts 'Creating basket:'
-basket = Basket.create()
+puts 'Creating baskets:'
+
+puts '   > basket 1'
+basket1 = Basket.create()
+
+puts '   > basket 2'
+basket2 = Basket.create(discount: 0.1, shipment: 7)
+
 
 puts "Creating user's choices:"
 
-puts '   > user choice 1'
-UsersChoice.create!(basket:, item: item1)
+puts '   > item 1 in basket 1'
+UsersChoice.create!(basket: basket1, item: item1)
 
-puts '   > user choice 2'
-UsersChoice.create!(basket:, item: item3)
+puts '   > item 2 in basket 1'
+UsersChoice.create!(basket: basket1, item: item3)
 
-# puts 'Done!'
+puts '   > item 1 in basket 2'
+UsersChoice.create!(basket: basket2, item: item1)
 
-# puts
-# puts 'Here are the items:'
-# Item.all.each do |item|
-#   puts "#{item.id +1} at a price of #{item.price}€ and quantity of #{item.quantity}"
+puts '   > item 2 in basket 2'
+UsersChoice.create!(basket: basket2, item: item2)
 
-# end
+puts '   > item 3 in basket 2'
+UsersChoice.create!(basket: basket2, item: item3)
+
+puts 'Done!'

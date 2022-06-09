105991c37406d6b0f11a637d67d5d0e7a6381ef2
TimothÃee Boiteau <timotheeboiteau@gmail.com>
2022-06-09 16:56:05 +0200

    Updated seeds for the Basket model to be able to calculate total before_save

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
-puts "Destroying all data"
+puts "\nSeeding database with 'db/seeds.rb'"
+puts "\nDestroying all remaining data"
 UsersChoice.destroy_all
 Basket.destroy_all
 Item.destroy_all
 
 
-puts 'Creating items:'
+puts "\nCreating items:"
 
 puts '   > item 1'
 item1 = Item.create(price: 15, quantity: 2)
@@ -16,16 +17,16 @@ puts '   > item 3'
 item3 = Item.create(price: 5, quantity: 4)
 
 
-puts 'Creating baskets:'
+puts "\nInitializing baskets:"
 
 puts '   > basket 1'
-basket1 = Basket.create()
+basket1 = Basket.new()
 
 puts '   > basket 2'
-basket2 = Basket.create(discount: 0.1, shipment: 7)
+basket2 = Basket.new(discount: 0.1, shipment: 7)
 
 
-puts "Creating user's choices:"
+puts "\nCreating user's choices:"
 
 puts '   > item 1 in basket 1'
 UsersChoice.create!(basket: basket1, item: item1)
@@ -42,4 +43,14 @@ UsersChoice.create!(basket: basket2, item: item2)
 puts '   > item 3 in basket 2'
 UsersChoice.create!(basket: basket2, item: item3)
 
-puts 'Done!'
+
+puts "\nSaving baskets:"
+
+puts '   > basket 1'
+basket1.save!
+
+puts '   > basket 2'
+basket2.save!
+
+
+puts "\nDone!"

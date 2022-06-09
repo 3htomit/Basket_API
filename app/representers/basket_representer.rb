0c70e25c8d41e18daa9c9bc15f8f351cfb22024f
TimothÃee Boiteau <timotheeboiteau@gmail.com>
2022-06-09 16:55:25 +0200

    Removed calculate_total from BasketRepresenter and used Basket instance total instead

--- app/representers/basket_representer.rb
+++ app/representers/basket_representer.rb
@@ -3,29 +3,13 @@ class BasketRepresenter
     @basket = basket
   end
 
-  def calculate_total(items, **options)
-    total = 0
-    vat = 1.1
-    items.each do |item|
-      total += item.price * item.quantity
-    end
-    if total.zero?
-      total=0
-    else
-      total *= (1 - (options[:discount] || 0))
-      total *= vat
-      total += (options[:shipment] || 5)
-    end
-    total = total.round(2)
-  end
-
   def as_json
     basket_data = {
       basket_id: 0,
       items: [],
       discount: 0,
       shipment: 5,
-      total: 0
+      total: basket.total
     }
     basket.items.each do |item|
       basket_data[:items].push({
@@ -37,7 +21,6 @@ class BasketRepresenter
     basket_data[:basket_id] = basket.id
     basket_data[:discount] = basket.discount unless basket.discount.nil?
     basket_data[:shipment] = basket.shipment unless basket.shipment.nil?
-    basket_data[:total] = calculate_total(basket.items, discount: basket.discount, shipment: basket.shipment)
     basket_data
   end
 
